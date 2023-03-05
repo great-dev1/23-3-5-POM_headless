47853c4ef0cb2a4abd63e528618bc315e8a67350
great-dev1 <twentytwentyaugust@outlook.com>
2023-03-05 04:48:25 -0800

    crud

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { AppProps } from "next/app";
 import { Refine } from "@pankod/refine-core";
 import routerProvider from "@pankod/refine-nextjs-router";
 import { dataProvider } from "@pankod/refine-supabase";
+import { HeadlessInferencer } from "@pankod/refine-inferencer/headless";
 import { authProvider } from "src/authProvider";
 import { supabaseClient } from "src/utility";
 
@@ -12,6 +13,15 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
       routerProvider={routerProvider}
       dataProvider={dataProvider(supabaseClient)}
       authProvider={authProvider}
+      resources={[
+        {
+            name: "products",
+            list: HeadlessInferencer,
+            show: HeadlessInferencer,
+            create: HeadlessInferencer,
+            edit: HeadlessInferencer,
+        },
+      ]}
     >
       <Component {...pageProps} />
     </Refine>
