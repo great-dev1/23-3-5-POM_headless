50929544b82917b07853aac93a9d83b86d69b3ea
great-dev1 <twentytwentyaugust@outlook.com>
2023-03-05 04:56:40 -0800

    update supabase key

--- src/utility/supabaseClient.ts
+++ src/utility/supabaseClient.ts
@@ -2,7 +2,7 @@ import { createClient } from "@pankod/refine-supabase";
 
 const SUPABASE_URL = "https://yugvqjtbebyqngdpstws.supabase.co";
 const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1Z3ZxanRiZWJ5cW5nZHBzdHdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc5OTY1NzgsImV4cCI6MTk5MzU3MjU3OH0.mzbcAXcKzidIObQr_2LNfKPmZo8KI6hHJlYe937A57s";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1Z3ZxanRiZWJ5cW5nZHBzdHdzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3Nzk5NjU3OCwiZXhwIjoxOTkzNTcyNTc4fQ.YKsP7ludiZ7jhp3f-3fcqVGL8eZeMDbfekWsKC67p7w";
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
   db: {
