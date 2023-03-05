729283b7b64bafa984d9f16fe3bd25e698f36456
Tom2187End <tomorrow2187end@outlook.com>
2023-03-05 04:41:34 -0800

    config supabase

--- src/utility/supabaseClient.ts
+++ src/utility/supabaseClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@pankod/refine-supabase";
 
-const SUPABASE_URL = "https://iwdfzvfqbtokqetmbmbp.supabase.co";
+const SUPABASE_URL = "https://yugvqjtbebyqngdpstws.supabase.co";
 const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDU2NzAxMCwiZXhwIjoxOTQ2MTQzMDEwfQ._gr6kXGkQBi9BM9dx5vKaNKYj_DJN1xlkarprGpM_fU";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1Z3ZxanRiZWJ5cW5nZHBzdHdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc5OTY1NzgsImV4cCI6MTk5MzU3MjU3OH0.mzbcAXcKzidIObQr_2LNfKPmZo8KI6hHJlYe937A57s";
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
   db: {
